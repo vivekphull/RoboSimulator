6a71eee193e4770f07ef313c7fd2f607641c40d6
Vivek Phull <vivek.phull@student.adelaide.edu.au>
2019-09-08 13:03:52 +0930

    Turn Right and Direction functionality included.

--- src/Game.java
+++ src/Game.java
@@ -2,25 +2,91 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
+    private static int x = 0;
+    private static int y = 0;
+    private static int MAX_X = 5;
+    private static int MAX_Y = 5;
+    private static int MIN_X = 0;
+    private static int MIN_Y = 0;
+
+
+    private enum Direction {NORTH, EAST, SOUTH, WEST}
+    private static int newDirection = 3;
+    private static String direction = "";
+    // taking String array input from user
+    private static Scanner sc = new Scanner(System.in);
+    private static int length = 4;
+    // create a String array to save user input
+    private static String[] input = new String[4];
+
+    public static void startGame () {
+        // loop over array to save user input
+        System.out.println("Please enter array elements: \n");
+        for (int i = 0; i < length; i++) {
+            String userInput = sc.next();
+            input[i] = userInput;
+        }
+        System.out.println("The String array input from user is : ");
+        System.out.println(Arrays.toString(input));
+//         testing the initial input command
+        if (input[0].equalsIgnoreCase("Start"))
+            System.out.print("\nOk");
+//        x = Integer.parseInt(input[1]);
+//        y = Integer.parseInt(input[2]);
+//        Direction newDirection = Direction.valueOf(input[3]);
+
+    }
+
+    public static void playGame(){
+        x = Integer.parseInt(input[1]);
+        y = Integer.parseInt(input[2]);
+        Direction newDirection = Direction.valueOf(input[3]);
+        System.out.print(Direction.valueOf(input[3]));
+        System.out.println("\nGive a Command (Move, Left, Right, Report)");
+        String userInput = sc.next();
+        if (userInput.equalsIgnoreCase("left"))
+                direction = "left";
+        else if (userInput.equalsIgnoreCase("right"))
+            direction = "right";
+        System.out.print("\n\n\n :" );
+
+        ReportLocation();
+    }
+    public static  void ReportLocation(){
+        x = Game.x;
+        y = Game.y;
+        System.out.println("The Current Location is : ");
+        System.out.print("X  : " + x + " And ");
+        System.out.print("Y  : " + y + "And ");
+        System.out.print("Direction is : " + newDirection + "\n");
+    }
+
+    public static void turnLeft(){
+        System.out.println(newDirection);
+        if (MIN_X < x && x <MAX_X){
+           x= x-1;
+
+        }else
+        {
+            System.out.println("Sorry, this move is not allowed.!");
+        }
+    }
+
+    public static void turnRight(){
+//        x = Game.x;
+            if (newDirection == 3)
+                newDirection = 0;
+            else
+                newDirection = newDirection -1;
+            System.out.println("\nNew Direction is " + newDirection);
+
+    }
+
 
     public static void main(String[] args) {
-        // taking String array input from user
-         Scanner sc = new Scanner(System.in);
-         int length = 4;
-         // create a String array to save user input
-         String[] input = new String[4];
-
-         // loop over array to save user input
-         System.out.println("Please enter array elements: \n");
-         for (int i = 0; i < length; i++) {
-             String userInput = sc.next();
-             input[i] = userInput;
-         }
-         System.out.println("The String array input from user is : ");
-         System.out.println(Arrays.toString(input));
-//         testing the initial input command
-         if (input[0].equals("Start"))
-             System.out.print("\nOk");
+        startGame();
+        playGame();
+        turnRight();
     }
 }
 
